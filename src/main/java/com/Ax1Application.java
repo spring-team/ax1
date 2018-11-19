249a985800c2cf7a23294b1e0198be759a888faa
Rod Johnson <johnsonroda@gmail.com>
2018-11-19 13:57:08 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/Ax1Application.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ax1Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Ax1Application.class, args);
+	}
+
+}
